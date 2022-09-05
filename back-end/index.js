a980d99396b01a6c9d6f7f7589e63049fc2cd2f4
Francisco Antonio Paiva <fcooantoniops@gmail.com>
2022-09-05 16:07:46 -0300

    POST/messages ready

--- back-end/index.js
+++ back-end/index.js
@@ -15,6 +15,12 @@ const participantSchema = joi.object({
   name: joi.string().required()
 });
 
+const messageSchema = joi.object({
+  to: joi.string().required(),
+  text: joi.string().required(),
+  type: joi.string().valid('message', 'private_message'),
+});
+
 //POST/participants
 app.post('/participants', async (req, res) => {
   const participant = req.body;
@@ -54,16 +60,51 @@ app.post('/participants', async (req, res) => {
 //GET/participants
 app.get('/participants', async (req, res) => {
   try {
-      const mongoClient = new MongoClient(process.env.MONGO_URI)
-      await mongoClient.connect()
+    const mongoClient = new MongoClient(process.env.MONGO_URI)
+    await mongoClient.connect()
 
-      const participantsCollection = mongoClient.db('bate-papo-uol').collection('participants');
-      const participants = await participantsCollection.find({}).toArray();
+    const participantsCollection = mongoClient.db('bate-papo-uol').collection('participants');
+    const participants = await participantsCollection.find({}).toArray();
 
-      mongoClient.close();
-      res.send(participants);
+    mongoClient.close();
+    res.send(participants);
   } catch (error) {
-      res.sendStatus(500);
+    res.sendStatus(500);
+  }
+});
+
+//POST/messages
+app.post('/messages', async (req, res) => {
+  const message = req.body;
+  const from = req.headers.user;
+  const validation = messageSchema.validate(message);
+  if (validation.error) {
+    return res.sendStatus(422);
+  }
+
+  try {
+    const mongoClient = new MongoClient(process.env.MONGO_URI)
+    await mongoClient.connect()
+
+    const participantsCollection = mongoClient.db('bate-papo-uol').collection('participants');
+    const messagesCollection = mongoClient.db('bate-papo-uol').collection('messages');
+
+    const registeredParticipant = await participantsCollection.findOne({name: from});
+    if (!registeredParticipant){
+      return res.sendStatus(422);
+    }
+
+    await messagesCollection.insertOne({
+      ...message,
+      from,
+      time: dayjs().format('HH:mm:ss')
+    });
+
+    mongoClient.close();
+    res.sendStatus(201);
+  } 
+  catch (error) {
+    res.sendStatus(500);
   }
 });
 
